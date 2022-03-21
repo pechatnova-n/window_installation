ea62a5d93a30ad987291abc77012b3c850fc5cf1
Natalia Pechatnova <pechatnova.n@yandex.ru>
2022-03-22 01:33:07 +0300

    delete slider module

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { form } from "./modules/form";
 import { validation } from "./modules/validation";
 import { calculator } from "./modules/calculator";
 import { swiper } from "./modules/swiper";
-import { slider } from "./modules/slider";
 import { scroll } from "./modules/scroll";
 import { tabs } from "./modules/tabs";
 
@@ -34,6 +33,5 @@ form({formId: "form2",
 validation();
 calculator();
 swiper();
-slider();
 scroll();
 tabs();
